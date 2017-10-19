023f986b4b4cd37c9547d2fa459370ad649141fd
Roman Iakobchuk <r.iakobchuk@gmail.com>
2017-10-19 19:39:51 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import registerServiceWorker from './registerServiceWorker'
+
+ReactDOM.render(<div />, document.getElementById('root'))
+registerServiceWorker()
