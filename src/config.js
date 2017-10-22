61ced49889002b29587d9e2954e728bb9125b20c
Andrei_Khamedov <xamedow@gmail.com>
2017-10-22 19:17:55 +0400

    Replace firebase access credentials with my own;

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase'
 
-export const appName = 'advreact-1610'
+export const appName = 'advreact-1610-8d439'
 
 firebase.initializeApp({
-    apiKey: "AIzaSyB31xpTtp4Jln_hb2kAbE4PGf6Mi8EgLyA",
+    apiKey: "AIzaSyDkx19f92qbT84fwaDMgZzi-wfglxtLk60",
     authDomain: `${appName}.firebaseapp.com`,
     databaseURL: `https://${appName}.firebaseio.com`,
     projectId: appName,
     storageBucket: "",
-    messagingSenderId: "397157634637"
+    messagingSenderId: "868897417787"
 })
